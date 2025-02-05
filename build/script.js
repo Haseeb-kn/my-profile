ce9d1466c963c413ed9d0630b02d5ae44d1f7ee2
Haseeb Khan <haseebskn@gmail.com>
2025-02-05 12:06:00 +0500

    Add files via upload

---
+++ build/script.js
@@ -0,0 +1,5 @@
+// Change background color on button click
+function changeBgColor() {
+    document.body.style.backgroundColor = 
+        document.body.style.backgroundColor === "white" ? "#f4f4f4" : "white";
+}
