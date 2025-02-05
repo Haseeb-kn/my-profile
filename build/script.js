a0b6cfefc206ff97b20a96f96f2de8cc77384246
Haseeb Khan <haseebskn@gmail.com>
2025-02-05 12:16:17 +0500

    Delete build directory

--- build/script.js
+++
@@ -1,5 +0,0 @@
-// Change background color on button click
-function changeBgColor() {
-    document.body.style.backgroundColor = 
-        document.body.style.backgroundColor === "white" ? "#f4f4f4" : "white";
-}
